f85637a1d85f9c2a7d97c3eb8b75fd41a408b296
tca88 <trishnaamin@gmail.com>
2019-08-12 18:26:38 -0400

    Complete selection and bubble sort algorithms. Run test_iterative.py to make sure everything works as expected.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,5 @@
-# TO-DO: Complete the selection_sort() function below 
+# TO-DO: Complete the selection_sort() function below
+height = [65, 48, 60, 54, 32, 89, 72] 
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -6,7 +7,10 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+        for n in range(i+1, len(arr)):
+            if arr[n] < arr[smallest_index]:
+                smallest_index = n
+        arr[cur_index],arr[smallest_index]=arr[smallest_index],arr[cur_index]
 
 
 
@@ -16,15 +20,25 @@ def selection_sort( arr ):
 
 
     return arr
-
+selection_sort(height)
+for h in height:
+    print(h)
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j -1]: # while we're not at the right spot (we're in the right place if the one below it is less)
+            arr[j], arr[j - 1] = arr[j - 1], arr[j] #swap
+            j -= 1
+            #back up and look at the previous spot
 
     return arr
-
+bubble_sort(height)
+for x in height:
+    print(x)
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+# def count_sort( arr, maximum=-1 ):
 
-    return arr
+#     return arr
